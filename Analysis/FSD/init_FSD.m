8663333bb6ef0293f5bf004a88692bc2d162c4bb
Christopher Horvat <christopher_horvat@brown.edu>
2024-05-15 17:21:35 -0400

    now working on making a modular code for adding IS2 processes

---
+++ Analysis/FSD/init_FSD.m
@@ -0,0 +1,6 @@
+% Initialization of FSD code. 
+
+all_floelengths_0 = [];
+all_floeids_0 = [];
+all_floe_seglengths_0 = [];
+all_usable_floes = [];
---
+++ Analysis/WAVES/init_WAVES.m
@@ -0,0 +1,11 @@
+% Initialization of wave code. 
+
+height_moving_avg = [];
+height_moving_std = [];
+moving_en = [];
+ssh = [];
+ssh_moving_std = [];
+ssh_neighbors = [];
+moving_neg = [];
+moving_pos = [];
+moving_ssh_no = [];
