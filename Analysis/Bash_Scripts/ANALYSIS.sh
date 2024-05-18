6cec817d8b4621ebf43a5542eb93a4fd87e2d9da
christopher_horvat@brown.edu <chorvat@node1901.oscar.ccv.brown.edu>
2024-05-18 09:59:43 -0400

    cleaning

--- Analysis/Bash_Scripts/ANALYSIS.sh
+++ Analysis/Bash_Scripts/ANALYSIS.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-#SBATCH -t 8:00:00
-#SBATCH -n 8
+#SBATCH -t 48:00:00
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -p batch
 #SBATCH --mem=0
 #SBATCH --account=epscor-condo
 #SBATCH --mail-user=bndnchrs@gmail.com 
@@ -10,6 +12,10 @@
 # #SBATCH --constraint=skylake
 # #SBATCH --exclusive
 
-matlab-threaded -r drive_analysis
+module load matlab
+
+cd /gpfs/data/epscor/chorvat/IS2/IS2-Gridded-Products/Analysis/Drivers/
+matlab-threaded -r "drive_analysis, exit"
+
 
 
